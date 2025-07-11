fa69deec733ddb5e0c7902ef17f2abe8dbf7ba75
Roromo0306 <pownyrainbow03@gmail.com>
2025-07-11 20:16:16 +0200

    Script MainMenu

--- TemporalJam/Assets/Scripts/MainMenuController.cs
+++ TemporalJam/Assets/Scripts/MainMenuController.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject MainMenu;
+    public GameObject Tutorial;
+    public GameObject Creditos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Creditos.SetActive(false);
+        Tutorial.SetActive(false);
+        MainMenu.SetActive(true);
     }
 
     // Update is called once per frame
-    void Update()
+    public void Comenzar()
     {
-        
+        //cambio de escena
+    }
+
+    public void AbrirCreditos()
+    {
+        Creditos.SetActive(true);
+        Tutorial.SetActive(false);
+        MainMenu.SetActive(false);
+    }
+
+    public void AbrirTutorial()
+    {
+        Creditos.SetActive(false);
+        Tutorial.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+
+    public void AbrirMenu()
+    {
+        Creditos.SetActive(false);
+        Tutorial.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
