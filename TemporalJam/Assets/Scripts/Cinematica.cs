00e212a8d58939686d9f6b8d24d0d330bf28b5ac
Roromo0306 <pownyrainbow03@gmail.com>
2025-07-11 21:23:10 +0200

    Transiciones

--- TemporalJam/Assets/Scripts/Cinematica.cs
+++ TemporalJam/Assets/Scripts/Cinematica.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Cinematica : MonoBehaviour
 {
     public Animator animator;
+    public Animator animator2;
     // Start is called before the first frame update
     void Start()
     {
         animator.SetTrigger("FadeOut");
+    }
+
+    public void Comenzar()
+    {
+
+        StartCoroutine(Iniciar());
+    }
+
+    IEnumerator Iniciar()
+    {
+        animator2.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Mini_001");
+
+
     }
 
     // Update is called once per frame
