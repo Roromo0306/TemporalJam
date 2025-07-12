4d8db93fb884ef98cd45ae8c51263f70cf74c2df
Alonso2880 <a80alonso@gmail.com>
2025-07-12 17:42:03 +0200

    hola

--- TemporalJam/Assets/Scripts/ColisionBorde.cs
+++ TemporalJam/Assets/Scripts/ColisionBorde.cs
@@ -9,7 +9,7 @@ public class ColisionBorde : MonoBehaviour
 
     private SpriteRenderer Barra;
 
-    [HideInInspector] public bool r = false, b = false, y = false, g=false;
+    [HideInInspector] public bool r = false, b = false, y = false, g=false, ry=false, rb = false, rg =false, by=false, bg=false, yg=false;
     public int combo = 0;
 
     public float tiempoCaida = 0.5f;
@@ -28,31 +28,91 @@ public class ColisionBorde : MonoBehaviour
     //Funcion que cambia el color de la barra al pulsar una tecla
     private void CambioColor()
     {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow)) //Azul
         {
             Barra.color = Color.cyan;
             b = true;
             StartCoroutine(VueltaBlanco());
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) //Rojo
         {
             Barra.color = Color.red;
             r = true;
             StartCoroutine(VueltaBlanco());
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow)) //Amarillo
         {
             Barra.color = Color.yellow;
             y = true;
             StartCoroutine(VueltaBlanco());
         }
 
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        if(Input.GetKeyDown(KeyCode.UpArrow)) //Verde
         {
             Barra.color = Color.green;
             g = true;
+            StartCoroutine(VueltaBlanco());
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && Input.GetKeyDown(KeyCode.RightArrow)) //rojo-amarillo
+        {
+            Color c;
+            ColorUtility.TryParseHtmlString("#FFA500", out c); //Naranja
+            Barra.color = c;
+  
+            ry = true;
+            StartCoroutine(VueltaBlanco());
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && Input.GetKeyDown(KeyCode.DownArrow)) //rojo-azul
+        {
+            Color c;
+            ColorUtility.TryParseHtmlString("#D207F6", out c); //violeta
+            Barra.color = c;
+
+            rb = true;
+            StartCoroutine(VueltaBlanco());
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && Input.GetKeyDown(KeyCode.UpArrow)) //rojo-verde
+        {
+            Color c;
+            ColorUtility.TryParseHtmlString("#CBB22A", out c); //Amarillo raro
+            Barra.color = c;
+
+            rg = true;
+            StartCoroutine(VueltaBlanco());
+        }
+
+        if(Input.GetKeyDown(KeyCode.DownArrow) && Input.GetKeyDown(KeyCode.RightArrow)) //azul-amarillo
+        {
+            Color c;
+            ColorUtility.TryParseHtmlString("#8BD765", out c); //Verde raro
+            Barra.color = c;
+
+            by = true;
+            StartCoroutine(VueltaBlanco());
+        } 
+
+        if(Input.GetKeyDown(KeyCode.DownArrow) && Input.GetKeyDown(KeyCode.UpArrow)) //azul-verde
+        {
+            Color c;
+            ColorUtility.TryParseHtmlString("#14F69E", out c); //Verde azulado
+            Barra.color = c;
+
+            bg = true;
+            StartCoroutine(VueltaBlanco());
+        }
+
+        if(Input.GetKeyDown(KeyCode.RightArrow) && Input.GetKeyDown(KeyCode.UpArrow)) //amarillo-verde
+        {
+            Color c;
+            ColorUtility.TryParseHtmlString("#B1FF00", out c); //Amarillo verdoso
+            Barra.color = c;
+
+            yg = true;
             StartCoroutine(VueltaBlanco());
         }
     }
