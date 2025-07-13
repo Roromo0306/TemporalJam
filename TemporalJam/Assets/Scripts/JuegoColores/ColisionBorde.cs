71fdea84815b876e505c320ef933d76a037fb728
Alonso2880 <a80alonso@gmail.com>
2025-07-13 03:39:31 +0200

    .

--- TemporalJam/Assets/Scripts/JuegoColores/ColisionBorde.cs
+++ TemporalJam/Assets/Scripts/JuegoColores/ColisionBorde.cs
@@ -127,7 +127,7 @@ public class ColisionBorde : MonoBehaviour
                 Barra.color = Color.green;
                 g = true;
                 StartCoroutine(VueltaBlanco());
-                return;
+                return; 
             }
         }
 
